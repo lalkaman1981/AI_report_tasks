be4f76b58eebf41adaa48e8c453db34e6a99864e
Khrystya Korets <hkorets0411@gmail.com>
2024-04-20 21:31:00 +0300

    Prompt 2 (ChatGpt)
    Оптимізуй цей код не змінюючи алгоритму

--- code optimization/read_write_file/read_write_file.py
+++ code optimization/read_write_file/read_write_file.py
@@ -1,53 +1,44 @@
 import urllib.request
 
 def read_input_file(url_link: str, number: int) -> list[list[str]]:
-  """
-  (str, int) -> (list(list))
-  Preconditions: 0 <= number <= 77
+    """
+    (str, int) -> (list(list))
+    Preconditions: 0 <= number <= 77
+    
+    Return list of strings lists from url
+    
+    >>> read_input_file \
+('https://raw.githubusercontent.com/anrom7/Test_Olya/master/New%20folder/total.txt',1)
+    [['1', 'Мацюк М. І.', '+', '197.859', '10.80']]
+    >>> read_input_file ('https://raw.githubusercontent.com/anrom7/\
+Test_Olya/master/New%20folder/total.txt',3)
+    [['1', 'Мацюк М. І.', '+', '197.859', '10.80'], ['2', 'Проць О. В.', '+', '197.152', '11.60'], \
+['3', 'Лесько В. О.', '+', '195.385', '10.60']]
+    """
+    with urllib.request.urlopen(url_link) as file:
+        file = file.readlines()
+    file = [line.decode('utf-8').strip() for line in file]
 
-  Return list of strings lists from url
+    if number == 0:
+        return []
 
-  >>> read_input_file('https://raw.githubusercontent.com/anrom7/Test_Olya/master/New%20folder/total.txt', 1)
-  [['1', 'Мацюк М. І.', '+', '197.859', '10.80']]
-  >>> read_input_file('https://raw.githubusercontent.com/anrom7/Test_Olya/master/New%20folder/total.txt', 3)
-  [['1', 'Мацюк М. І.', '+', '197.859', '10.80'], ['2', 'Проць О. В.', '+', '197.152', '11.60'], ['3', 'Лесько В. О.', '+', '195.385', '10.60']]
-  """
+    grade_ = []
+    res = []
 
-  with urllib.request.urlopen(url_link) as file:
-    lines = file.readlines()
+    plus_ = [line.split('\t')[1] for line in file if line.startswith('—')]
+    grade_ = [line.split()[-1] for line in file if line.startswith('Середній')]
+    lines = [line.split('\t') for line in file if line[0].isdigit()]
+    res = [[line[0], line[1], plus_[i], line[3], grade_[i]] for i, line in enumerate(lines) if i < number]
 
-  if number == 0:
-    return []
-
-  if 0 < number <= 77 and isinstance(url_link, str) and isinstance(number, int):
-    result = []
-    for line in lines:
-      decoded_line = line.decode('utf-8').strip()
-      if not decoded_line:  # Skip empty lines for efficiency
-        continue
-
-      if decoded_line[0] == '—':
-        plus = " ".join(decoded_line.split('\t')[1:2])
-      elif decoded_line.split()[0] == 'Середній':
-        grade = " ".join([decoded_line.split()[-1]])
-      elif decoded_line[0].isdigit():
-        num, name = " ".join(decoded_line.split('\t')[:2]).split(' ', 1)
-        nmt = " ".join(decoded_line.split('\t')[3:4])
-        result.append([num, name, plus, nmt, grade])
-
-  return result[:number]
-
+    return res
 
 def write_csv_file(url: str):
-  """
-  str -> csv
-  """
-  data = read_input_file(url, 77)
-
-  with open("total.csv", "w", encoding="utf-8") as result:
-    start_line = "№,ПІБ,Д,Заг.бал,С.б.док.осв."
-    result.write(start_line)
-    for line in data:
-      new_line = "\n" + ",".join(line)
-      result.write(new_line)
+    """
+    str -> csv
+    """
+    with open("total.csv", "w", encoding= "utf-8") as result:
+        start_line = "№,ПІБ,Д,Заг.бал,С.б.док.осв."
+        result.write(start_line)
+        for line in read_input_file(url, 77):
+            result.write("\n" + ",".join(line))
 
